0f370b6c94b3d2c817b99315ddb033383c716169
Mindtable <mtble@mail.ru>
2022-05-11 14:31:16 +0300

    add submodule opencv

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "iostream"
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
 //#include "opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
 
 int main() {
-    Mat img = imread("Baby_fish.jpeg");
+    Mat img = imread("../Baby_fish.jpeg");
     if (img.empty()) {
         cout << "Error" << endl;
         return -1;
