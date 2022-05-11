3a602a3c9a3c76f52f9dbdc47ccd6421d8f36153
Mindtable <mtble@mail.ru>
2022-05-11 09:52:55 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include <opencv2/highgui/highgui.hpp>
+//#include "opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat img = imread("Baby_fish.jpeg");
+    if (img.empty()) {
+        cout << "Error" << endl;
+        return -1;
+    }
+    imshow("Lena", img);
+    waitKey();
+    return 0;
+}
